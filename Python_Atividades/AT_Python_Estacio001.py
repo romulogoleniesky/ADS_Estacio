9b3bdda84235d5620713cf4169894cf10f60f32d
RomuloGoleniesky <goleniesky@hotmail.com>
2021-03-24 19:05:22 -0300

    AT_001
    Realizado a atividade 001 de Python.

---
+++ Python_Atividades/AT_Python_Estacio001.py
@@ -0,0 +1,11 @@
+## Curso de Análise e Desenvolvimento de Sistemas - Estácio de Sá.
+## PARADIGMAS DE LING. DE PROGRAMAÇÃO EM PYTHON (EEX0130/3734132) 9002
+## Aluno: RÔMULO GOLENIESKY
+
+## Atividade - 001 (AT_Python_Estacio001) - Print na tela.
+
+print("Hello Word!")
+
+
+saudacao = "Olá mundo!"
+print(saudacao)
---
+++ Python_Atividades/AT_Python_Estacio002.py
@@ -0,0 +1,6 @@
+## Curso de Análise e Desenvolvimento de Sistemas - Estácio de Sá.
+## PARADIGMAS DE LING. DE PROGRAMAÇÃO EM PYTHON (EEX0130/3734132) 9002
+## Aluno: RÔMULO GOLENIESKY
+
+## Atividade - 002 (AT_Python_Estacio002) - Input.
+
