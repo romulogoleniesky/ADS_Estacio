f48d4861c787a6963499574b128d06046a855cb7
RomuloGoleniesky <goleniesky@hotmail.com>
2021-03-24 19:27:32 -0300

    Create AT_Python_Estacio003.py
    Feito a atividade do IMC em 3 linhas.

---
+++ Python_Atividades/AT_Python_Estacio003.py
@@ -0,0 +1,9 @@
+## Curso de Análise e Desenvolvimento de Sistemas - Estácio de Sá.
+## PARADIGMAS DE LING. DE PROGRAMAÇÃO EM PYTHON (EEX0130/3734132) 9002
+## Aluno: RÔMULO GOLENIESKY
+
+## Atividade - 003 (AT_Python_Estacio003) - IMC.
+
+peso = float(input("Olá vamos verificar seu IMC, qual é o seu peso? "))
+altura = float(input("Agora diga qual é a sua altura? "))
+print("IMC: ", peso/altura**2)
