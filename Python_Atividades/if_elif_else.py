5baf575649e1139bf860357db80795443f3c6a68
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-15 12:43:59 -0300

    troca do nome da variável.

--- Python_Atividades/if_elif_else.py
+++ Python_Atividades/if_elif_else.py
@@ -1,7 +1,7 @@
-n=int(input('Qual é a sua idade? '))
-if n < 18:
-    print('Você é de Novinho!')
-elif n > 30:
+idade = int(input('Qual é a sua idade? '))
+if idade < 18:
+    print('Você é Novinho!')
+elif idade > 30:
     print('Você tá velho!')
 else:
     print('Você é de maior!')
