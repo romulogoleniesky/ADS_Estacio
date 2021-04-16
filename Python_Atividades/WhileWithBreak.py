71bea4d9d6fae3057bc1988f49e2426929a962c3
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-16 12:10:29 -0300

    WhileWithBreak.py created

---
+++ Python_Atividades/WhileWithBreak.py
@@ -0,0 +1,10 @@
+# While infinity used the 'Break'.
+# While With Break:
+import emoji
+while True:
+    senha = input(emoji.emojize('Digite a Senha:key:'))
+    if senha == 'exit':
+        break
+print("ACESSO CONFIRMADO")
+
+
