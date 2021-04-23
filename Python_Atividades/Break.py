56ed4226cb30e64373fc3f55b433aab9e6c9cd16
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-23 11:58:50 -0300

    Break.py created

---
+++ Python_Atividades/Break.py
@@ -0,0 +1,12 @@
+# testes com Break e Continue:
+
+for x in range(1,5):
+    if x == 4:
+        break
+    else:
+        print(x)
+for x in range(1,5):
+    if x == 4:
+        continue
+    else:
+        print(x)
