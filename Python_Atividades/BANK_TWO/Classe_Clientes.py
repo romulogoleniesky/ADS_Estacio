257cf112433986a81a8c3a3486c5805e819a083f
RomuloGoleniesky <goleniesky@hotmail.com>
2021-05-13 22:31:13 -0300

    criado o BANK_TWO

---
+++ Python_Atividades/BANK_TWO/Classe_Clientes.py
@@ -0,0 +1,5 @@
+class Conta:
+    def __init__(self, clientes, numero, saldo):
+        self.clientes = clientes
+        self.numero = numero
+        self.saldo = saldo
---
+++ Python_Atividades/BANK_TWO/Classe_Contas.py
@@ -0,0 +1,5 @@
+class Conta:
+    def __init__(self, cpf, nome, endereco):
+        self.cpf = cpf
+        self.nome = nome
+        self.endereco = endereco
